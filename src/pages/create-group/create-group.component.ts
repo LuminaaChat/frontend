023b817fc5a6ba0a8e9c0df73a89b68d6490ad57
Ohmert, Tasida <Tasida.Ohmert@adesso.de>
2023-11-19 13:27:44 +0100

    Mitglieder und Klienten hinzufügen und anzeigen

--- src/pages/create-group/create-group.component.ts
+++ src/pages/create-group/create-group.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-group',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './create-group.component.html',
   styleUrl: './create-group.component.scss'
 })
@@ -14,7 +15,27 @@ export class CreateGroupComponent {
 
   groupname: string = '';
   areas: string [] =['area1', 'area2'];
-  employees: string[] = [];
-  clients: string [] = [];
+  employees: string[] = ['Danny', 'Christoph'];
+  clients: string [] = ['Nils', 'Tasida'];
 
+  addedEmployee: string[] = [];
+  addedClient: string[] = [];
+
+  selectedEmployee: string = '';
+  selectedClient: string = '';
+
+  addEmployee(){
+    console.log(this.selectedEmployee);
+    
+    if(this.selectedEmployee){
+      this.addedEmployee.push(this.selectedEmployee)
+    }
+  }
+
+
+  addClient(){
+    if(this.selectedClient){
+      this.addedClient.push (this.selectedClient)
+    }
+  }
 }
