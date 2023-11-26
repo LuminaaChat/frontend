194b34dffd0a08d9eb438197e9533c8f04923bc5
Dennis Hering <dennis@hering.dev>
2023-11-26 13:13:56 +0100

    Add Users Api Stuff

---
+++ src/services/users-api.service.ts
@@ -0,0 +1,40 @@
+import {Injectable, Signal} from '@angular/core';
+import {environment} from "../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {toSignal} from "@angular/core/rxjs-interop";
+import {User} from "../types/user.type";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersApiService {
+  baseUrl = `${environment.url}${environment.apiVersion}/users`;
+
+  constructor(private http: HttpClient) { }
+
+  list(): Signal<User[] | undefined> {
+    const observable: Observable<User[]> = this.http.get<User[]>(this.baseUrl);
+    return toSignal(observable);
+  }
+
+  get(id: string): Signal<User | undefined> {
+    const observable: Observable<User> = this.http.get<User>(`${this.baseUrl}/${id}`);
+    return toSignal(observable);
+  }
+
+  create(entity: User): Signal<User | undefined> {
+    const observable: Observable<User> = this.http.post<User>(`${this.baseUrl}`, entity);
+    return toSignal(observable);
+  }
+
+  update(entity: User): Signal<User | undefined> {
+    const observable: Observable<User> = this.http.put<User>(`${this.baseUrl}/${entity._id}`, entity);
+    return toSignal(observable);
+  }
+
+  delete(id: string): Signal<User | undefined> {
+    const observable: Observable<User> = this.http.delete<User>(`${this.baseUrl}/${id}`);
+    return toSignal(observable);
+  }
+}
