2da4ab537b8fc8ee4ec1142532301636a7c5870c
Dennis Hering <dennis.hering@adesso.de>
2023-11-29 17:35:53 +0100

    place drawer in pages

--- src/app/pages/user-groups/user-groups.component.ts
+++ src/app/pages/user-groups/user-groups.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-user-groups',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './user-groups.component.html',
   styleUrl: './user-groups.component.scss'
 })
 export class UserGroupsComponent {
+  sidebar: boolean = false;
 
+  constructor() {}
+
+  toggleSidebar() {
+    this.sidebar = !this.sidebar;
+  }
 }
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
+  sidebar: boolean = false;
 
+  constructor() {}
+
+
+  toggleSidebar() {
+    this.sidebar = !this.sidebar;
+  }
 }
